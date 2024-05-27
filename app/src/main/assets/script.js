9f562e727944b386e17f74f3343d752f304698a0
Hamdy El-menshawi <141504750+hamdy-1111@users.noreply.github.com>
2024-05-27 03:11:12 +0300

    Update script.js

--- app/src/main/assets/script.js
+++ app/src/main/assets/script.js
@@ -1,23 +1,39 @@
 const filesDownloadButton = document.getElementById("file-downloads-button");
 const filesDownloadDialog = document.getElementById("files-download-dialog");
-const filesDownloadDialogBottomOk = document.getElementById("files-download-dialog-bottom-ok");
+const overlay = document.getElementById('overlay');
+const okButton = document.getElementById('files-download-dialog-bottom-ok');
 const filesDownloadList   = document.getElementById("files-download-list");
 
 const fileDownloadListItem = document.createElement("li");
 fileDownloadListItem.className = "files-download-list-item";
 
+
 filesDownloadButton.onclick = () => {
-    filesDownloadDialog.style.display = 'flex';
+    filesDownloadDialog.style.display = 'block';
+    overlay.style.display = 'block';
     requestAvailableDownloads();
 };
 
-filesDownloadDialogBottomOk.onclick = () => {
-    filesDownloadDialog.style.display = 'none';
+
+okButton.onclick = () => {
+    closeDialog();
 };
+
+overlay.onclick = () => {
+    closeDialog();
+};
+
+function closeDialog() {
+    filesDownloadDialog.style.display = 'none';
+    overlay.style.display = 'none';
+}
+
 
 
 
 function requestAvailableDownloads() {
+    // Show the loader when the request starts
+    showLoader();
     while(filesDownloadList.lastChild) {filesDownloadList.lastChild.remove();}
     fetch("/available-downloads", {
         headers: {
@@ -42,7 +58,14 @@ function requestAvailableDownloads() {
                 document.body.removeChild(link);
             })
         });
-    })
+        // Hide the loader when the request is complete
+        hideLoader();
+    }).catch(error => {
+        console.error('Error fetching available downloads:', error);
+
+        // Hide the loader even if there is an error
+        hideLoader();
+    });
 }
 
 function getFormattedFileSize(s) {
@@ -61,3 +84,27 @@ function getFormattedFileSize(s) {
     return `${s.toFixed(2)} ${levels[level]}`;
     
 }
+
+
+const loader = document.querySelector('.process');
+function showLoader() {
+    loader.style.display= 'block';
+}
+
+function hideLoader() {
+    document.querySelector('.plane').style.animation = 'plane-done 1.2s infinite';
+    setTimeout(() => {
+        loader.style.display = 'none';
+    }, 1000);
+}
+
+/* reloading page */
+window.addEventListener('onload', function (event) {
+    showLoader();
+    console.log('Page is unloading...');
+});
+
+window.addEventListener('load', function (event) {
+    hideLoader();
+    console.log('Page has fully loaded.');
+});
