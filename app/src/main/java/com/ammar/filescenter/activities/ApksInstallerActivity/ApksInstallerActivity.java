20c4400ed795ef41456de8918125b13790299602
Ammar64 <ammar_ramadan151515@hotmail.com>
2024-07-16 12:31:57 +0300

    Commiting before merge

--- app/src/main/java/com/ammar/filescenter/activities/ApksInstallerActivity/ApksInstallerActivity.java
+++ app/src/main/java/com/ammar/filescenter/activities/ApksInstallerActivity/ApksInstallerActivity.java
@@ -3,10 +3,14 @@ package com.ammar.filescenter.activities.ApksInstallerActivity;
 import static com.ammar.filescenter.activities.MainActivity.MainActivity.darkMode;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -23,6 +27,12 @@ public class ApksInstallerActivity extends AppCompatActivity {
 
     private LinearLayout logLayout;
     ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(), (result) -> {
+        String filename = getFileName(result);
+        if( !filename.endsWith(".apks") ) {
+            Toast.makeText(this, "Please choose apks file", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
         Intent intent = new Intent(this, PackageInstallerService.class);
         intent.setAction(Consts.ACTION_TRIGGER_APKS_INSTALL);
         intent.setData(result);
@@ -33,6 +43,8 @@ public class ApksInstallerActivity extends AppCompatActivity {
             logLayout.addView(makeLogTextView(log));
         });
     });
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         if( darkMode ) {
@@ -48,7 +60,15 @@ public class ApksInstallerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apksinstaller);
         logLayout = findViewById(R.id.LL_InstallerLogLayout);
-        mGetContent.launch("application/*");
+
+        if(getIntent().getData() == null) {
+            mGetContent.launch("*/*");
+        } else {
+            Intent intent = new Intent(this, PackageInstallerService.class);
+            intent.setData(getIntent().getData());
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            startService(intent);
+        }
     }
 
 
@@ -58,5 +78,27 @@ public class ApksInstallerActivity extends AppCompatActivity {
         //logText.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         logText.setText(text);
         return logText;
+    }
+
+    public String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, new String[]{OpenableColumns.DISPLAY_NAME} , null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
     }
 }
