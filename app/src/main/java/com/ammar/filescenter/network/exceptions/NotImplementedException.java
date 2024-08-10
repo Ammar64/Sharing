e550b5e34919f88164c6ed31a452c3ead648a0e5
Ammar64 <ammar_ramadan151515@hotmail.com>
2024-08-10 23:57:59 +0300

    Update applicationId

--- app/src/main/java/com/ammar/filescenter/network/exceptions/NotImplementedException.java
+++
@@ -1,7 +0,0 @@
-package com.ammar.filescenter.network.exceptions;
-
-public class NotImplementedException extends Exception {
-    public NotImplementedException(String message) {
-        super(message);
-    }
-}
