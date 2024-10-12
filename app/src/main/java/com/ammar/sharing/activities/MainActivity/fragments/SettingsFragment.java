452967a6b81c1396a17c8f1cff2383e9cc902a11
Ammar64 <ammar_ramadan151515@hotmail.com>
2024-10-12 22:43:43 +0300

    Fix text overflow in settings and make french translation shorter

--- app/src/main/java/com/ammar/sharing/activities/MainActivity/fragments/SettingsFragment.java
+++ app/src/main/java/com/ammar/sharing/activities/MainActivity/fragments/SettingsFragment.java
@@ -8,13 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import com.ammar.sharing.R;
@@ -32,7 +32,7 @@ public class SettingsFragment extends Fragment {
     private SwitchButton usersBlockS;
     private SwitchButton debugModeS;
 
-    private RelativeLayout languageRL;
+    private ConstraintLayout languageRL;
     private AlertDialog languageAD;
     private SharedPreferences settingsPref;
 
